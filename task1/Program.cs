98af9f90e0aca28223e3e475058c08b29635a6a6
Artem Zabrodenko <temazasport@mail.ru>
2022-11-18 15:58:13 +0800

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+
+int numA = SetNumber("A");
+
+int sum = GetSumAllToNumberA(numA);
+
+Print(sum);
+
+int SetNumber(string str)
+{
+
+    System.Console.WriteLine($"Введите {str}");
+    int num = int.Parse(Console.ReadLine());
+
+    return num;
+}
+
+int GetSumAllToNumberA(int numMetodA)
+{
+    int sum = 0;
+    for (int i = 0; i <= numMetodA; i++)
+    {
+        sum += i;
+    }
+     return sum;
+}
+
+void Print(int num)
+{
+    System.Console.WriteLine($"number = {num}");
+}
