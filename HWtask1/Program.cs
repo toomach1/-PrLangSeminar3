b3e39a311ecf6356871ace2166ee28ecc455fa1a
Artem Zabrodenko <temazasport@mail.ru>
2022-11-21 19:23:20 +0800

    HWtask1

---
+++ HWtask1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите метод, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int numA = SetNumber("число A");
+int numB = SetNumber("число B");
+int rais = RaisNumAtoPow(numA,numB);
+
+
+
+System.Console.WriteLine($"{numA} в степени {numB} = {rais}");
+
+int SetNumber(string str)
+{
+    System.Console.WriteLine($"Введите {str}");
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int RaisNumAtoPow(int numMetodA, int numMetodB)
+{
+    int multi = 1;
+    for (int i = 1; i <= numMetodB; i++)
+    {
+        multi = numMetodA * multi;
+    }
+    return multi;
+}
