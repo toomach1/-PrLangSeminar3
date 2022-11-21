4784862dfcf22017b29c7141b6ddb9bf4a2e8bbf
Artem Zabrodenko <temazasport@mail.ru>
2022-11-21 12:04:27 +0800

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+
+
+int num = SetNumber("число");
+
+int multi = 1;
+for (int i = 1; i <=num; i++)
+{
+    multi *= i;
+}
+
+System.Console.WriteLine(multi);
+
+
+
+
+int SetNumber(string str)
+{
+
+    System.Console.WriteLine($"Введите {str}");
+    num = int.Parse(Console.ReadLine());
+
+    return num;
+}
+
