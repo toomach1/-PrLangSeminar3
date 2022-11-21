3ae6bc0cc5bd7ffa39712a1f45a567f89ac05c4c
Artem Zabrodenko <temazasport@mail.ru>
2022-11-21 20:21:30 +0800

    Hwtask3

---
+++ Hwtask3/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] array = new int[8];
+
+Random myRandom = new Random();
+
+
+
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = myRandom.Next(0, 100);
+    Console.Write("{0} ", array[i]);
+}
