edca93ca3c57977fd42a83bf2e26c5044de61948
Artem Zabrodenko <temazasport@mail.ru>
2022-11-21 11:30:30 +0800

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,31 @@
+﻿// //Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+
+int NumA = SetNumber("число");
+
+int count = CountNum(NumA);
+
+System.Console.WriteLine($"количество цифр в числе = {count}");
+
+
+
+int SetNumber(string str)
+{
+
+    System.Console.WriteLine($"Введите {str}");
+    int num = int.Parse(Console.ReadLine());
+
+    return num;
+}
+
+int CountNum(int NumAMetod)
+{   
+    int count = 0;
+    while(NumAMetod>0)
+    {
+        
+        count ++ ;
+        NumAMetod = NumAMetod/10;
+    }
+    return count;
+}    
